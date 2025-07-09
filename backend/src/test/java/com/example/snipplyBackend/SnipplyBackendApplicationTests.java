7828b60dc0f25900ac1dfa8ed149f151c2c2db87
Pratyush-who <pratyushmehra2005@gmail.com>
2025-07-09 22:50:07 +0530

    back

---
+++ backend/src/test/java/com/example/snipplyBackend/SnipplyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.snipplyBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnipplyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
